4a19bc13aec9027e1e0184aeeeadca29dd6bd6b5
GreatnessBarthNathan <ekemeziebartholomew@gmail.com>
2024-08-10 15:23:56 +0100

    updated constants

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -201,7 +201,7 @@ export const prayerGroups = [
   {
     group: 22,
     leader: "Bro Fisayo",
-    expectedCount: 27,
+    expectedCount: 28,
   },
   {
     group: 23,
@@ -231,171 +231,24 @@ export const prayerGroups = [
   {
     group: 28,
     leader: "Bishop Phil",
-    expectedCount: 32,
+    expectedCount: 30,
   },
   {
     group: 29,
     leader: "Bro Ohis Okhuoya",
-    expectedCount: 32,
+    expectedCount: 30,
   },
   {
     group: 30,
     leader: "Bishop OSD",
     expectedCount: 30,
   },
+  {
+    group: 31,
+    leader: "Bro Isaiah Bicko",
+    expectedCount: 30,
+  },
 ]
-// export const prayerGroups = [
-//   {
-//     group: "Group 1",
-//     leader: "Bishop Tesla",
-//     expectedCount: 24,
-//   },
-//   {
-//     group: "Group 2",
-//     leader: "Sis Edith Tesla",
-//     expectedCount: 18,
-//   },
-//   {
-//     group: "Group 3",
-//     leader: "Bishop Damian",
-//     expectedCount: 23,
-//   },
-//   {
-//     group: "Group 4",
-//     leader: "Bro Aluya Collins",
-//     expectedCount: 23,
-//   },
-//   {
-//     group: "Group 5",
-//     leader: "Sis Ahmed Shaafat",
-//     expectedCount: 18,
-//   },
-//   {
-//     group: "Group 6",
-//     leader: "Bro Nathan Ekemezie",
-//     expectedCount: 19,
-//   },
-//   {
-//     group: "Group 7",
-//     leader: "Bishop Graham",
-//     expectedCount: 19,
-//   },
-//   {
-//     group: "Group 8",
-//     leader: "Sis Vera Graham",
-//     expectedCount: 19,
-//   },
-//   {
-//     group: "Group 9",
-//     leader: "BCN",
-//     expectedCount: 26,
-//   },
-//   {
-//     group: "Group 10",
-//     leader: "Bro Emmanuel Onuoha",
-//     expectedCount: 25,
-//   },
-//   {
-//     group: "Group 11",
-//     leader: "Bro Aneto Omo-Ataga",
-//     expectedCount: 19,
-//   },
-//   {
-//     group: "Group 12",
-//     leader: "Bishop Greatness",
-//     expectedCount: 16,
-//   },
-//   {
-//     group: "Group 13",
-//     leader: "Bro Francis Uzuegbu",
-//     expectedCount: 29,
-//   },
-//   {
-//     group: "Group 14",
-//     leader: "Bishop Wealth",
-//     expectedCount: 13,
-//   },
-//   {
-//     group: "Group 15",
-//     leader: "Bro Enoch Chidozie",
-//     expectedCount: 16,
-//   },
-//   {
-//     group: "Group 16",
-//     leader: "Bishop Harold",
-//     expectedCount: 17,
-//   },
-//   {
-//     group: "Group 17",
-//     leader: "Bro Timothy Onoyumo",
-//     expectedCount: 17,
-//   },
-//   {
-//     group: "Group 18",
-//     leader: "Bro Festus Azobua",
-//     expectedCount: 18,
-//   },
-//   {
-//     group: "Group 19",
-//     leader: "Bro Emmanuel Okotie",
-//     expectedCount: 21,
-//   },
-//   {
-//     group: "Group 20",
-//     leader: "Bro Chukwunyem Onyekpe",
-//     expectedCount: 22,
-//   },
-//   {
-//     group: "Group 21",
-//     leader: "Sis Sandra Igbinovia",
-//     expectedCount: 31,
-//   },
-//   {
-//     group: "Group 22",
-//     leader: "Bro Fisayo",
-//     expectedCount: 27,
-//   },
-//   {
-//     group: "Group 23",
-//     leader: "Bishop EmShaw",
-//     expectedCount: 18,
-//   },
-//   {
-//     group: "Group 24",
-//     leader: "Bro Ovo Atibaka",
-//     expectedCount: 32,
-//   },
-//   {
-//     group: "Group 25",
-//     leader: "Bro Chuka Onyekpe",
-//     expectedCount: 31,
-//   },
-//   {
-//     group: "Group 26",
-//     leader: "Bro Kunle Asaolu",
-//     expectedCount: 31,
-//   },
-//   {
-//     group: "Group 27",
-//     leader: "Bishop PEE",
-//     expectedCount: 28,
-//   },
-//   {
-//     group: "Group 28",
-//     leader: "Bishop Phil",
-//     expectedCount: 32,
-//   },
-//   {
-//     group: "Group 29",
-//     leader: "Bro Ohis Okhuoya",
-//     expectedCount: 32,
-//   },
-//   {
-//     group: "Group 30",
-//     leader: "Bishop OSD",
-//     expectedCount: 30,
-//   },
-// ]
 
 export const hours = [
   "01",
