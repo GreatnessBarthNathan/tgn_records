f51dc6595c0a3fe88c78e8b57130be7ac27f1906
GBN <ekemeziebartholomew@gmail.com>
2026-07-17 18:26:02 +0100

    .

--- server.js
+++ server.js
@@ -39,8 +39,8 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
-app.use(express.static(path.resolve(__dirname, './public')))
-// app.use(express.static(path.resolve(__dirname, "./client/dist")))
+// app.use(express.static(path.resolve(__dirname, './public')))
+app.use(express.static(path.resolve(__dirname, './client/dist')))
 
 app.use(cookieParser())
 app.use(express.json())
@@ -58,12 +58,12 @@ app.use('/api/v1/user', authenticateUser, userRouter)
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/rc', authenticateUser, rcRouter)
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './public', 'index.html'))
-})
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"))
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, './public', 'index.html'))
 // })
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'))
+})
 
 app.use('*', (req, res) => {
   res.status(404).json({ msg: 'not found' })
